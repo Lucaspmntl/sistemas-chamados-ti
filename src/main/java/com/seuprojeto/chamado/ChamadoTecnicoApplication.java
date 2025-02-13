7c9559525d2b7b715432c8f5264ff2cfae111191
Lucas Pimentel <lucaspimentel808@gmail.com>
2025-02-13 18:26:35 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/seuprojeto/chamado/ChamadoTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.chamado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadoTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadoTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seuprojeto/chamado/ChamadoTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.chamado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadoTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
